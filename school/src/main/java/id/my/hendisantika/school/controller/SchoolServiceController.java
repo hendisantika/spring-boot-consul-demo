7a5fbf24a6b8d3ac1c80629891d146bffa48a747
hendisantika <hendisantika@yahoo.co.id>
2025-01-16 06:20:24 +0700

    Add getStudents in SchoolServiceController🫸🌀✏️📗🐧🐳⬆

--- school/src/main/java/id/my/hendisantika/school/controller/SchoolServiceController.java
+++ school/src/main/java/id/my/hendisantika/school/controller/SchoolServiceController.java
@@ -1,6 +1,10 @@
 package id.my.hendisantika.school.controller;
 
 import id.my.hendisantika.school.delegate.StudentServiceDelegate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 06.19
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class SchoolServiceController {
 
     private final StudentServiceDelegate studentServiceDelegate;
+
+    @GetMapping(value = "/getSchoolDetails/{schoolname}")
+    public String getStudents(@PathVariable String schoolname) {
+        log.info("Going to call student service to get data!");
+        return studentServiceDelegate.callStudentServiceAndGetData(schoolname);
+    }
+
 }
