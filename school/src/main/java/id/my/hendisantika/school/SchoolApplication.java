aaadb0dc5dd4e9e39924e62448fc65eb4d3b6f9a
hendisantika <hendisantika@yahoo.co.id>
2025-01-16 06:19:05 +0700

    Add EnableDiscoveryClient🫸🌀✏️📗🐧🐳⬆

--- school/src/main/java/id/my/hendisantika/school/SchoolApplication.java
+++ school/src/main/java/id/my/hendisantika/school/SchoolApplication.java
@@ -2,7 +2,12 @@ package id.my.hendisantika.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SchoolApplication {
 
@@ -10,4 +15,9 @@ public class SchoolApplication {
         SpringApplication.run(SchoolApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
